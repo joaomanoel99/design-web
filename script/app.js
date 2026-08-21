e49987e6c2dc557b8719cd4f288d4cb495708c63
joaomnoel99 <souza.manoel@escolar.ifrn.edu.br>
2026-08-21 13:40:47 -0300

    segund mudança do java

--- script/app.js
+++ script/app.js
@@ -1,1 +1,12 @@
 alert("Olá Mundo")
+
+var nasc = 2010;
+let nome = "João";
+const viva = true;
+let altura = 1.80;
+
+if (viva) {
+    let saudacao = "Olá, " + nome + "! " ;
+    let msg = `Altura: ${altura}m  | Idade: ${2026 - nasc}`;
+    alert(saudacao + n/ msg);
+}
