4060567e0ee3ec402dfa2f582dfb7d03f7552197
joaomnoel99 <souza.manoel@escolar.ifrn.edu.br>
2026-08-26 17:56:21 -0300

    novo 3

---
+++ app3.js
@@ -0,0 +1,15 @@
+let nasc = 2010;
+let nome = "João";
+let viva = true;
+let altura = 1.79;
+let sexo =;
+let peso = null;
+let imc = "peso"*10;
+
+alert(`Ano de nascimento - tipo ${typeof nasc}}`);
+alert(`Nome - tipo ${typeof nome}}`);
+alert(`Viva - tipo ${typeof viva}}`);
+alert(`Altura - tipo ${typeof altura}}`);
+alert(`Sexo - tipo ${typeof sexo}}`);
+alert(`Peso - tipo ${typeof peso}}`);
+alert(`IMC - tipo ${typeof imc}}`);
